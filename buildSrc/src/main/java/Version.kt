b5a82f9bfd76f9e0e649fa6561d7b3edf347456c
Michel Gauzins <michel.gauzins@dailymotion.com>
2018-11-20 15:41:51 +0100

    Clean build Add EVENT_AD_PAUSE, EVENT_AD_PLAY Sync EVENT_AD_PAUSE, EVENT_AD_PLAY with mPlayWhenReady to match current state

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -2,5 +2,5 @@ object Version {
     val targetSdkVersion = 28
     val minSdkVersion = 14
     val compileSdkVersion = 28
-    val buildToolsVersion = "26.0.3"
+    val buildToolsVersion = "28.0.3"
 }
