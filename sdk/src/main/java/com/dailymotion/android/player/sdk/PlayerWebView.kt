b01c707676b729d721ac24346732596cb28a31c9
Geoffrey TAN <geoffrey.tan@dailymotion.com>
2020-03-31 14:41:41 +0200

    Disabled Jetifier since we only use AndroidX libraries and use correct google play service dependencies to get AdvertisingId

--- sdk/src/main/java/com/dailymotion/android/player/sdk/PlayerWebView.kt
+++ sdk/src/main/java/com/dailymotion/android/player/sdk/PlayerWebView.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
-import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.net.Uri
