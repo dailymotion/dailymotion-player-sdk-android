ea0adafcec33317da698a174026424bdc2f8d0bb
Andrada Anca <>
2022-03-21 11:51:49 +0200

    Updated the naming of tracking para,eters.

--- sdk/src/main/java/com/dailymotion/android/player/sdk/PlayerWebView.kt
+++ sdk/src/main/java/com/dailymotion/android/player/sdk/PlayerWebView.kt
@@ -771,10 +771,8 @@ class PlayerWebView @JvmOverloads constructor(
                     "omsdk" to OMHelper.getVersion(),
                     "ompartner" to OMHelper.PARTNER_NAME,
                     "omversion" to OMHelper.PARTNER_VERSION,
-                    "tracking" to mapOf(
-                        "reader.advertising.id" to adInfo?.id.orEmpty(),
-                        "reader.lmt" to (adInfo?.isLimitAdTrackingEnabled ?: true)
-                    )
+                    "deviceId" to adInfo.id.orEmpty(),
+                    "limitAdTracking" to (adInfo?.isLimitAdTrackingEnabled ?: true)
                 )
             )
         ).toString()
